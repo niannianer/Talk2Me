bf5265c970f29eabd356cbc29e26763fccb8fce8
raoliqin <627974327@qq.com>
2018-10-11 22:54:10 +0800

    teacher detail page

--- pages/teacherDetail/index.js
+++ pages/teacherDetail/index.js
@@ -5,7 +5,44 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    commentList: [{
+      title: '风趣，知识丰富',
+      name: '王晓晓',
+      time: '2016年2月12日',
+      type: '商务英语',
+      comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet.'
+    }, {
+      title: '风趣，知识丰富',
+      name: '王晓晓',
+      time: '2016年2月12日',
+      type: '商务英语',
+      comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet.'
+    }],
+    courseList: [{
+      dayOne: '周一',
+      dayTwo: '周二',
+      dayThree: '周三',
+      dayFour: '周四',
+      dayFive: '周五',
+      daySix: '周六',
+      daySeven: '周日'
+    },{
+        dayOne: '12.00--14.00',
+        dayTwo: '',
+        dayThree: '10.00-11.00',
+        dayFour: '9.00-12.00',
+        dayFive: '',
+        daySix: '11.00-12.00',
+        daySeven: '11.00-12.00'
+      }, {
+        dayOne: '',
+        dayTwo: '12.00--14.00',
+        dayThree: '10.00-11.00',
+        dayFour: '',
+        dayFive: '',
+        daySix: '11.00-12.00',
+        daySeven: '11.00-12.00'
+      }]
   },
 
   /**
